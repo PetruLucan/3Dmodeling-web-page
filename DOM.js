11760d0c9e2f9b8bb5e0f2b27c3e835ee13f1109
PetruLucan <lucanpetru11@gmail.com>
2019-10-08 15:26:56 +0300

    DOM work/ Add-swich content from menu

--- DOM.js
+++ DOM.js
@@ -1,9 +1,34 @@
-var navBarEl = document.getElementById("navBar");
+var getHomeContentEl = document.getElementById('homeContent');
+var getPreviewContentEl = document.getElementById('previewContent');
+var getProjectsContentEl = document.getElementById('projectsContent');
+var getContactsContentEl = document.getElementById('contactsContent');
+var allLiElements = document.getElementById('meniu').children;
+var getContentEl = document.getElementById('contentWrapper').children;
+var test = document.getElementById('test');
+// var testContent = document.getElementById('testContent');
 
 
-navBarEl = document.addEventListener("click", onClick);
 
-function onClick(){
-    console.log(navBarEl)
-    navBarEl.classList.add("active");
+
+
+
+function onMenuClick(el) {
+
+    for (let i = 0; i < allLiElements.length; i++) {
+        allLiElements[i].className = "inactive";
+
+    }
+    el.className = "active";
+
+    onClick()
+
+}
+function onClick() {
+    for (let j = 0; j < getContentEl; j++) {
+        getContentEl[i].className = 'hidden';
+
+    }
+    getContentEl.className = "content";
+    // testContent.classList.add('content')
+
 }
