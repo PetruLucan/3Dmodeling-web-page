d0d6cff63025c2dd71d542711de6fe6d55e10cb9
PetruLucan <lucanpetru11@gmail.com>
2019-11-01 18:22:59 +0200

    restyled, +responsive

--- DOM.js
+++ DOM.js
@@ -39,6 +39,31 @@ function toggle(target) {
     return false;
 }
 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 +0.5}s`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+
 
 
 
